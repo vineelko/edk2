d261a4b2c3a22b48d502a0403db3cd18fb11cabd
Bret Barkelew <brbarkel@microsoft.com>
2024-07-01 14:22:14 -0700

    MdeModulePkg: HiiDatabaseDxe: Fill in the current background
    - Set pallette pointers to NULL
    
    Signed-off-by: Vineel Kovvuri[MSFT] <vineelko@microsoft.com>

--- MdeModulePkg/Universal/HiiDatabaseDxe/Font.c
+++ MdeModulePkg/Universal/HiiDatabaseDxe/Font.c
@@ -2127,7 +2127,10 @@ HiiStringToImage (
     if ((Flags & EFI_HII_DIRECT_TO_SCREEN) == EFI_HII_DIRECT_TO_SCREEN) {
       BltBuffer = NULL;
       if (RowInfo[RowIndex].LineWidth != 0) {
+        // Fill in the current background
+        UINT32  Bkgnd = Background.Blue | Background.Green << 8 | Background.Red << 16;
         BltBuffer = AllocatePool (RowInfo[RowIndex].LineWidth * RowInfo[RowIndex].LineHeight * sizeof (EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
+        SetMem32 (BltBuffer, RowInfo[RowIndex].LineWidth * RowInfo[RowIndex].LineHeight * sizeof (EFI_GRAPHICS_OUTPUT_BLT_PIXEL), Bkgnd);
         if (BltBuffer == NULL) {
           Status = EFI_OUT_OF_RESOURCES;
           goto Exit;
